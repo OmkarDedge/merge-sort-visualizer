040630ac889349e77330cc6abfa9abdeedb68a30
OmkarDedge <123588944+OmkarDedge@users.noreply.github.com>
2024-06-15 13:40:46 -0700

    Update SortingVisualizer.jsx

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -2,16 +2,9 @@ import React from "react";
 import { getMergeSortAnimations } from "../SortingAlgorithms/sortingAlgorithms";
 import "./SortingVisualizer.css";
 
-// Change this value for the speed of the animations.
 const ANIMATION_SPEED_MS = 3;
-
-// Change this value for the number of bars (value) in the array.
 const NUMBER_OF_ARRAY_BARS = 310;
-
-// This is the main color of the array bars.
 const PRIMARY_COLOR = "turquoise";
-
-// This is the color of array bars that are being compared throughout the animations.
 const SECONDARY_COLOR = "red";
 
 export default class SortingVisualizer extends React.Component {
@@ -63,7 +56,11 @@ export default class SortingVisualizer extends React.Component {
     const { array } = this.state;
 
     return (
-      <div>
+      <div className="sorting-visualizer">
+        <div className="button-container">
+          <button onClick={() => this.resetArray()}>Generate New Array</button>
+          <button onClick={() => this.mergeSort()}>Merge Sort</button>
+        </div>
         <div className="array-container">
           {array.map((value, idx) => (
             <div
@@ -75,17 +72,11 @@ export default class SortingVisualizer extends React.Component {
             ></div>
           ))}
         </div>
-        <div className="button-container">
-          <button onClick={() => this.resetArray()}>Generate New Array</button>
-          <button onClick={() => this.mergeSort()}>Merge Sort</button>
-        </div>
       </div>
     );
   }
 }
 
-// From https://stackoverflow.com/questions/4959975/generate-random-number-between-two-numbers-in-javascript
 function randomIntFromInterval(min, max) {
-  // min and max included
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
